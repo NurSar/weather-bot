50938248526473d0ec83f83fb4a45bd1112e4eff
NurSar <nuraly.sarsembay@gmail.com>
2025-08-07 14:28:10 +0500

    testing new VS api

--- test/test_weather_api.py
+++ test/test_weather_api.py
@@ -3,13 +3,22 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-API_KEY = os.getenv('OPENWEATHER_API_KEY')
+API_KEY = os.getenv('VISUALCROSSING_API_KEY')
 lat = os.getenv('LATITUDE')
 lon = os.getenv('LONGITUDE')
 
 def check_api_status():
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+    # url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+    # url = (
+    #     f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+    #     f"{lat}%2C%20{lon}?unitGroup=metric&include=hours&key={API_KEY}&contentType=json"
+    # )
+    url = (
+        f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+        f"{lat}%2C%20{lon}/yesterday?unitGroup=metric&include=hours&key={API_KEY}&contentType=json"
+    )
     response = requests.get(url)
+    print(response.json())
     print(f"Status code: {response.status_code}")
     if response.status_code == 200:
         print("API is working!")
